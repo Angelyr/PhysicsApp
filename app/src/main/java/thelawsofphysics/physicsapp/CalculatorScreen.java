6ed21853e2df097f982997ae65d91754137a1547
h1aih <haih2@rpi.edu>
2017-10-05 14:03:28 -0400

    added default calculator UI

---
+++ app/src/main/java/thelawsofphysics/physicsapp/CalculatorScreen.java
@@ -0,0 +1,16 @@
+package thelawsofphysics.physicsapp;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by h1aih on 2017/10/5.
+ * Screen for calculator
+ */
+
+public class CalculatorScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator_screen);
+    }
+}
