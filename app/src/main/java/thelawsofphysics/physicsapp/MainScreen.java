8e18e1032cb0410e6d7b49c5ab2939c1aee41c39
Angel <scardking@gmail.com>
2017-10-12 15:15:33 -0400

    Added UI to show equations

--- app/src/main/java/thelawsofphysics/physicsapp/MainScreen.java
+++ app/src/main/java/thelawsofphysics/physicsapp/MainScreen.java
@@ -1,7 +1,10 @@
 package thelawsofphysics.physicsapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        Button equationsBtn = (Button)findViewById(R.id.equationsBtn);
+        equationsBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), EquationsScreen.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
