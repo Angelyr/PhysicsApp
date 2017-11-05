75a1beb0383434c34386fe33cc8be94b2fdd903a
h1aih <haih2@rpi.edu>
2017-11-04 23:21:27 -0400

    added default calculator UI

--- app/src/main/java/thelawsofphysics/physicsapp/CalculatorScreen.java
+++ app/src/main/java/thelawsofphysics/physicsapp/CalculatorScreen.java
@@ -67,6 +67,63 @@ public class CalculatorScreen extends AppCompatActivity {
                 Double num2 = Double.parseDouble(secondNumEditText.getText().toString());
                 Double result= num1 / num2;
                 resultTextview.setText(result+"");
+
+            }
+        });
+        Button pwrbtn= (Button) findViewById(R.id.PowerButton);
+        pwrbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                EditText firstNumEditText= (EditText) findViewById(R.id.FirstNumEditText);
+                EditText secondNumEditText= (EditText) findViewById(R.id.SecondNumEditText);
+                TextView resultTextview = (TextView) findViewById(R.id.resultTextView);
+                Double num1 = Double.parseDouble(firstNumEditText.getText().toString());
+                Double num2 = Double.parseDouble(secondNumEditText.getText().toString());
+                Double result=Math.pow(num1,num2);
+                resultTextview.setText(result+"");
+
+            }
+        });
+        Button logbtn= (Button) findViewById(R.id.LogButton);
+        logbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                EditText firstNumEditText= (EditText) findViewById(R.id.FirstNumEditText);
+                EditText secondNumEditText= (EditText) findViewById(R.id.SecondNumEditText);
+                TextView resultTextview = (TextView) findViewById(R.id.resultTextView);
+                Double num1 = Double.parseDouble(firstNumEditText.getText().toString());
+                Double num2 = Double.parseDouble(secondNumEditText.getText().toString());
+                Double result=Math.log(num2)/Math.log(num1);
+                resultTextview.setText(result+"");
+
+            }
+        });
+        Button sinbtn= (Button) findViewById(R.id.SinButton);
+        sinbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                EditText firstNumEditText= (EditText) findViewById(R.id.FirstNumEditText);
+                EditText secondNumEditText= (EditText) findViewById(R.id.SecondNumEditText);
+                TextView resultTextview = (TextView) findViewById(R.id.resultTextView);
+                Double num1 = Double.parseDouble(firstNumEditText.getText().toString());
+                Double num2 = Double.parseDouble(secondNumEditText.getText().toString());
+                Double result=Math.sin(num1);
+                resultTextview.setText(result+"");
+
+            }
+        });
+        Button cosbtn= (Button) findViewById(R.id.CosButton);
+        cosbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                EditText firstNumEditText= (EditText) findViewById(R.id.FirstNumEditText);
+                EditText secondNumEditText= (EditText) findViewById(R.id.SecondNumEditText);
+                TextView resultTextview = (TextView) findViewById(R.id.resultTextView);
+                Double num1 = Double.parseDouble(firstNumEditText.getText().toString());
+                Double num2 = Double.parseDouble(secondNumEditText.getText().toString());
+                Double result=Math.cos(num1);
+                resultTextview.setText(result+"");
+
             }
         });
     }
