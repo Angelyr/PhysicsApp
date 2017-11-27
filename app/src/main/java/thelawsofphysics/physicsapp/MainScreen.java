1f04fe8854a19e6e4c9825a1f8f9f51f1a884633
Angel <scardking@gmail.com>
2017-11-26 20:36:22 -0500

    Fixed Errors. Fixed SimSearch button.

--- app/src/main/java/thelawsofphysics/physicsapp/MainScreen.java
+++ app/src/main/java/thelawsofphysics/physicsapp/MainScreen.java
@@ -4,9 +4,19 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 import android.support.v7.widget.Toolbar;
-import android.view.*;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 //import java.util.List;
 
 public class MainScreen extends AppCompatActivity {
@@ -97,6 +107,45 @@ public class MainScreen extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent startIntent = new Intent(getApplicationContext(), ProblemSearch.class);
+                startActivity(startIntent);
+            }
+        });
+
+        Button favoriteBtn = (Button)findViewById(R.id.FavoriteBtn);
+        favoriteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), Favorite.class);
+                startActivity(startIntent);
+            }
+        });
+
+        Button recentlyBtn = (Button)findViewById(R.id.recentlyBtn);
+        recentlyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), List.class);
+                //read from input file into list
+                try {
+                    ArrayList<String> equations = new ArrayList<>();
+                    String aEquation;
+                    FileInputStream inputStream = openFileInput("Recent");
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    while ((aEquation = bufferedReader.readLine())!=null)
+                    {
+                        equations.add(0, aEquation);
+                    }
+                    list = equations.toArray(new String[equations.size()]);
+                    startIntent.putExtra("display", list);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //creates list
                 startActivity(startIntent);
             }
         });
